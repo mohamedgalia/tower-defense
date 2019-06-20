17ef15454c8ba4b703fbb7fcb73b2ef2d0124546
davidginzbourg <davidginzbourg@gmail.com>
2019-06-20 23:03:13 +0300

    Fix project requirements (#82)

--- install_tower_defense_ubuntu.sh
+++ install_tower_defense_ubuntu.sh
@@ -2,8 +2,4 @@
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-<<<<<<< HEAD
-sudo apt-get install python3.6 -y
-=======
-sudo apt-get install python3.6 -y
->>>>>>> b83d194... Installations for Windows and Linux (issue #3)+ updating README.md (issue #23)
+sudo apt-get install -y python3-pip python3.6
--- run_game_linux.sh
+++ run_game_linux.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
 python3 main.py
-=======
-python3 main.py
->>>>>>> b83d194... Installations for Windows and Linux (issue #3)+ updating README.md (issue #23)
