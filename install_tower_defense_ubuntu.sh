4df1988c56699131a684946bffb109e92a1f01a3
davidginzbourg <davidginzbourg@gmail.com>
2019-06-20 23:03:47 +0300

    Fix README

--- install_tower_defense_ubuntu.sh
+++ install_tower_defense_ubuntu.sh
@@ -3,3 +3,4 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install -y python3-pip python3.6
+pip install -rrequirements.txt
