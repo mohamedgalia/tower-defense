07210a158e67a0ca803b2bce99e9acf50198fda5
mohamedgalia <48689046+mohamedgalia@users.noreply.github.com>
2019-06-20 15:40:20 +0300

    cache of boards to not play in the same board
    add a cache to boards to make the player have all the boards

--- Boards.py
+++ Boards.py
@@ -98,10 +98,16 @@ board5 = [[0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 
 boards = [board0, board1, board2, board3, board4, board5]
 
+cache = []
 
 class BoardsClass:
-
     @staticmethod
     def GetBoard():
         board = random.choice(boards)
+        while board in cache:
+            board = random.choice(boards)
+        if(len(cache) == 4):
+            cache.remove(boards[0])
+        cache.append(board)
         return board
+        
